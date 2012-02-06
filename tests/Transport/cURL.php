c2806728bbcaf27e1a7df238fd841ca4556e0574
Ryan McCue <me@ryanmccue.info>
2012-02-06 23:20:37 +1000

    Reset the transport after testing

--- tests/Transport/cURL.php
+++ tests/Transport/cURL.php
@@ -4,4 +4,8 @@ class RequestsTest_Transport_cURL extends RequestsTest_Transport_Base {
 	public function setUp() {
 		Requests::$transport = 'Requests_Transport_cURL';
 	}
+
+	public function tearDown() {
+		Requests::$transport = null;
+	}
 }
--- tests/Transport/fsockopen.php
+++ tests/Transport/fsockopen.php
@@ -4,4 +4,8 @@ class RequestsTest_Transport_fsockopen extends RequestsTest_Transport_Base {
 	public function setUp() {
 		Requests::$transport = 'Requests_Transport_fsockopen';
 	}
+
+	public function tearDown() {
+		Requests::$transport = null;
+	}
 }
