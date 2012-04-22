627128a2f40ef01d792186f0118cd13f7e133949
Ryan McCue <me@ryanmccue.info>
2012-04-22 15:53:11 +1000

    If OpenSSL isn't loaded, HTTPS requests should fail nicely
    Travis CI recently changed their 5.2 instances, so the tests began failing. This isn't an error with the library, but rather with the testing.

--- tests/Transport/fsockopen.php
+++ tests/Transport/fsockopen.php
@@ -2,4 +2,13 @@
 
 class RequestsTest_Transport_fsockopen extends RequestsTest_Transport_Base {
 	protected $transport = 'Requests_Transport_fsockopen';
+
+	public function testHTTPS() {
+		// If OpenSSL isn't loaded, this should fail
+		if (!defined('OPENSSL_VERSION_NUMBER')) {
+			$this->setExpectedException('Requests_Exception');
+		}
+
+		return parent::testHTTPS();
+	}
 }
