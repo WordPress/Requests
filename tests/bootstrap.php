bacf847b98d6ddafae7f0c95f12bbb32e36b2d1a
Ryan McCue <me@ryanmccue.info>
2012-07-03 16:45:22 +1000

    Add fake request_multiple for RawTransport too

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -109,6 +109,15 @@ class RawTransport implements Requests_Transport {
 	public function request($url, $headers = array(), $data = array(), $options = array()) {
 		return $this->data;
 	}
+	public function request_multiple($requests, $options) {
+		foreach ($requests as $id => &$request) {
+			$handler = new RawTransport();
+			$handler->data = $request['options']['raw.data'];
+			$request = $handler->request($request['url'], $request['headers'], $request['data'], $request['options']);
+		}
+
+		return $requests;
+	}
 	public static function test() {
 		return true;
 	}
