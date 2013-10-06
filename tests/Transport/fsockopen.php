4e2ceb603d154c40c3cffd5bc712ad7fa7927267
Ryan McCue <me@ryanmccue.info>
2013-10-06 20:00:25 +1000

    Ensure Tests_Transport_*::setUp() is public
    Should fix the tests broken on merge.

--- tests/Transport/fsockopen.php
+++ tests/Transport/fsockopen.php
@@ -4,7 +4,7 @@ class RequestsTest_Transport_fsockopen extends RequestsTest_Transport_Base {
 	protected $transport = 'Requests_Transport_fsockopen';
 
 	protected $skip_https = false;
-	protected function setUp() {
+	public function setUp() {
 		// If OpenSSL isn't loaded, this should fail
 		if (!defined('OPENSSL_VERSION_NUMBER')) {
 			$this->skip_https = true;
