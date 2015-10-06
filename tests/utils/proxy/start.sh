89cffe3a8165de51f58b617adeb035337a7b893d
Ryan McCue <me@ryanmccue.info>
2015-10-06 15:20:54 +1000

    Remove pipe from $ARGS

--- tests/utils/proxy/start.sh
+++ tests/utils/proxy/start.sh
@@ -2,7 +2,7 @@ PROXYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PORT=${PORT:-9000}
 
 PROXYBIN=${PROXYBIN:-"$(which mitmdump)"}
-ARGS="-s '$PROXYDIR/proxy.py' -p $PORT > $PROXYDIR/proxy.log"
+ARGS="-s '$PROXYDIR/proxy.py' -p $PORT"
 PIDFILE="$PROXYDIR/proxy.pid"
 
 start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --exec $PROXYBIN -- $ARGS
