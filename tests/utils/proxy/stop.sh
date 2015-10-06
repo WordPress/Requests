a59b2c53deedbf81326e6ef3ce4ba43aa229b10a
Ryan McCue <me@ryanmccue.info>
2015-10-06 15:19:53 +1000

    Use correct variable name

--- tests/utils/proxy/stop.sh
+++ tests/utils/proxy/stop.sh
@@ -1,5 +1,5 @@
 PROXYDIR="$PWD/$(dirname $0)"
 
-PIDFILE="$SERVERDIR/proxy.pid"
+PIDFILE="$PROXYDIR/proxy.pid"
 
-start-stop-daemon --stop --pidfile $PIDFILE --make-pidfile && rm $SERVERDIR/proxy.pid
+start-stop-daemon --stop --pidfile $PIDFILE --make-pidfile && rm $PROXYDIR/proxy.pid
