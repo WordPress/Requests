e0ee23ec0bdbb0f8336952ce99e73a1e2353422c
Vivek Kumar <viveklakhera@gmail.com>
2016-06-03 23:47:59 +0530

    Closes #167 - Add json() for json responses
    * Add json() which returns decoded json from the response body
      if the response body is a valid json else throws an exception

--- src/Response.php
+++ src/Response.php
@@ -127,4 +127,30 @@ class Response {
 			throw new $exception(null, $this);
 		}
 	}
+
+	/**
+	 * Returns json decoded response
+	 *
+	 * @throws Requests_Exception If `$this->body` is not a valid json
+	 * @return array
+	 */
+	public function json($assoc = true, $depth = 512, $options = 0) {
+		static $json_errors = array(
+			JSON_ERROR_DEPTH => 'JSON_ERROR_DEPTH - Maximum stack depth exceeded',
+			JSON_ERROR_STATE_MISMATCH => 'JSON_ERROR_STATE_MISMATCH - Underflow or the modes mismatch',
+			JSON_ERROR_CTRL_CHAR => 'JSON_ERROR_CTRL_CHAR - Unexpected control character found',
+			JSON_ERROR_SYNTAX => 'JSON_ERROR_SYNTAX - Syntax error, malformed JSON',
+			JSON_ERROR_UTF8 => 'JSON_ERROR_UTF8 - Malformed UTF-8 characters, possibly incorrectly encoded'
+		);
+
+		$data = json_decode($this->body, $assoc, $depth, $options);
+
+		if (JSON_ERROR_NONE !== json_last_error()) {
+			$last_error = json_last_error();
+			$error = isset($json_errors[$last_error]) ? $json_errors[$last_error] : 'Unknown error';
+			throw new Requests_Exception('Unable to parse JSON data: ' . $error, 'response.invalid', $this);
+		}
+
+		return $data;
+	}
 }
