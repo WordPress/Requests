0469bc8b47a64623c2a73f039821b2d5f866278c
Gennady Kovshenin <gennady@kovshenin.com>
2018-02-17 12:44:16 +0500

    Force Content-Length for POST requests
    Addresses #248

--- library/Requests/Transport/fsockopen.php
+++ library/Requests/Transport/fsockopen.php
@@ -169,6 +169,12 @@ class Requests_Transport_fsockopen implements Requests_Transport {
 					$headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 				}
 			}
+			elseif($options['type'] === Requests::POST) {
+				if (!isset($case_insensitive_headers['Content-Length'])) {
+					// Prevent 411 errors from some servers
+					$headers['Content-Length'] = 0;
+				}
+			}
 		}
 
 		if (!isset($case_insensitive_headers['Host'])) {
--- tests/Transport/fsockopen.php
+++ tests/Transport/fsockopen.php
@@ -2,4 +2,15 @@
 
 class RequestsTest_Transport_fsockopen extends RequestsTest_Transport_Base {
 	protected $transport = 'Requests_Transport_fsockopen';
+
+	public function testContentLengthHeader() {
+		$hooks = new Requests_Hooks();
+		$hooks->register('fsockopen.after_headers', array($this, 'checkContentLengthHeader'));
+
+		$request = Requests::post(httpbin('/post'), array(), array(), $this->getOptions(array('hooks' => $hooks)));
+	}
+
+	public function checkContentLengthHeader($headers) {
+		$this->assertContains('Content-Length: 0', $headers);
+	}
 }
