13463b011d1d89f82bb1e439aef1e14e7ed8ba1b
jianmingLee <lijianmin520@gmail.com>
2018-08-03 15:28:26 +0800

    格式修复

--- library/Requests.php
+++ library/Requests.php
@@ -265,35 +265,35 @@ class Requests {
 	/**
 	 * Send a POST request/Support for multiple files, complex array parameters
 	 */
-        public static function post($url, $headers = [], $data = [], $files = [], $options = []) : \Requests_Response{
-            if (count($files)) {
-    
-                // replacing file paths with curlFile Object
-                array_walk($files, function ($filePath, $key) use (&$data) {
-                    if (is_array($filePath)) {
-                        array_walk($filePath, function ($subFilePath, $subKey) use (&$data, $key) {
-                            $data[$key][$subKey] = new \CURLFile(realpath($subFilePath));
-                        });
-                    } else {
-                        $data[ $key ] = new \CURLFile(realpath($filePath));
-                    }
-                });
-    
-                self::httpBuildQuery($data);
-    
-                // starting to add a hook to attach file to request
-                $hooks = new \Requests_Hooks();
-                $hooks->register('curl.before_send', function ($fp) use ($data) {
-                    curl_setopt($fp, CURLOPT_SAFE_UPLOAD, true);
-                    curl_setopt($fp, CURLOPT_POSTFIELDS, $data);
-                });
-                $options = ['hooks' => $hooks];
-                // no need to set the body, it's taken care of by hooks
-                $data = [];
-            }
-    
-            return self::request($url, $headers, $data, self::POST, $options);
+    public static function post($url, $headers = [], $data = [], $files = [], $options = []) : \Requests_Response{
+        if (count($files)) {
+
+            // replacing file paths with curlFile Object
+            array_walk($files, function ($filePath, $key) use (&$data) {
+                if (is_array($filePath)) {
+                    array_walk($filePath, function ($subFilePath, $subKey) use (&$data, $key) {
+                        $data[$key][$subKey] = new \CURLFile(realpath($subFilePath));
+                    });
+                } else {
+                    $data[ $key ] = new \CURLFile(realpath($filePath));
+                }
+            });
+
+            self::httpBuildQuery($data);
+
+            // starting to add a hook to attach file to request
+            $hooks = new \Requests_Hooks();
+            $hooks->register('curl.before_send', function ($fp) use ($data) {
+                curl_setopt($fp, CURLOPT_SAFE_UPLOAD, true);
+                curl_setopt($fp, CURLOPT_POSTFIELDS, $data);
+            });
+            $options = ['hooks' => $hooks];
+            // no need to set the body, it's taken care of by hooks
+            $data = [];
         }
+
+        return self::request($url, $headers, $data, self::POST, $options);
+    }
 	/**
 	 * Send a PUT request
 	 */
@@ -1005,15 +1005,15 @@ class Requests {
 		return false;
 	}
 	
-        protected static function httpBuildQuery(array &$data, $key = '', $value = null) {
-            foreach ($value ?? $data as $k => $v) {
-                if (is_array($v)) {
-                    self::httpBuildQuery($data, "{$key}[{$k}]", $v);
-                }else{
-                    $cur_key = $key ? "{$key}[{$k}]" : "{$k}";
-                    $data[$cur_key] = $v;
-                }
-                unset($data[$k]);
+    protected static function httpBuildQuery(array &$data, $key = '', $value = null) {
+        foreach ($value ?? $data as $k => $v) {
+            if (is_array($v)) {
+                self::httpBuildQuery($data, "{$key}[{$k}]", $v);
+            }else{
+                $cur_key = $key ? "{$key}[{$k}]" : "{$k}";
+                $data[$cur_key] = $v;
             }
+            unset($data[$k]);
         }
+    }
 }
