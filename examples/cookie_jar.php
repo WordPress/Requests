fd39b7600a445badd28b2490850b0370a8f41ec5
jrfnl <jrfnl@users.noreply.github.com>
2019-09-16 23:14:56 +0200

    PHP compatibility: don't use short arrays
    Short arrays were introduced in PHP 5.4.
    As this package is supposed to be compatible with PHP 5.2 and up, they cannot be used and IMO, even the example code should comply with that.

--- examples/cookie_jar.php
+++ examples/cookie_jar.php
@@ -7,13 +7,13 @@ include('../library/Requests.php');
 Requests::register_autoloader();
 
 // Say you need to fake a login cookie
-$c = new Requests_Cookie_Jar(['login_uid' =>  'something']);
+$c = new Requests_Cookie_Jar(array('login_uid' => 'something'));
 
 // Now let's make a request!
 $request = Requests::get(
 	'http://httpbin.org/cookies', // Url
-	[],  // No need to set the headers the Jar does this for us
-	['cookies' => $c] // Pass in the Jar as an option
+	array(),  // No need to set the headers the Jar does this for us
+	array('cookies' => $c) // Pass in the Jar as an option
 );
 
 // Check what we received
