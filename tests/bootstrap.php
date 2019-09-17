ebfc37cf539acbc41f52fdaee16147e3e21071e0
jrfnl <jrfnl@users.noreply.github.com>
2019-09-17 16:10:08 +0200

    CS: move test helper classes to their own file [2]
    * Add the new class file to the `tests/Mocks` directory.
    * Prefix the class, same as the other classes in the test directory.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -49,22 +49,3 @@ function httpbin($suffix = '', $ssl = false) {
 	$host = $ssl ? 'https://' . REQUESTS_TEST_HOST_HTTPS : 'http://' . REQUESTS_TEST_HOST_HTTP;
 	return rtrim($host, '/') . '/' . ltrim($suffix, '/');
 }
-
-class RawTransport implements Requests_Transport {
-	public $data = '';
-	public function request($url, $headers = array(), $data = array(), $options = array()) {
-		return $this->data;
-	}
-	public function request_multiple($requests, $options) {
-		foreach ($requests as $id => &$request) {
-			$handler       = new RawTransport();
-			$handler->data = $request['options']['raw.data'];
-			$request       = $handler->request($request['url'], $request['headers'], $request['data'], $request['options']);
-		}
-
-		return $requests;
-	}
-	public static function test() {
-		return true;
-	}
-}
