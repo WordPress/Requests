4a6283db567f33185fd5b6b2b53b4c946cbe1ba8
jrfnl <jrfnl@users.noreply.github.com>
2021-06-29 20:40:33 +0200

    Tests: namespace the `Cookies` test class
    This commit:
    * Namespaces the class.
    * Renames the `RequestsTest_Cookies` class to `CookiesTest`.
    * Renames the class file to match the new class name.
    * Add `use` statements for all used classes.
    * Removes the named reference to the old test class file from the PHPUnit config file.

--- tests/CookiesTest.php
+++ tests/CookiesTest.php
@@ -1,8 +1,17 @@
 <?php
 
-use Requests\Tests\TestCase;
+namespace Requests\Tests;
 
-class RequestsTest_Cookies extends TestCase {
+use Requests;
+use Requests\Tests\TestCase;
+use Requests_Cookie;
+use Requests_Cookie_Jar;
+use Requests_Exception;
+use Requests_IRI;
+use Requests_Response_Headers;
+use Requests_Utility_CaseInsensitiveDictionary;
+
+class CookiesTest extends TestCase {
 	public function testBasicCookie() {
 		$cookie = new Requests_Cookie('requests-testcookie', 'testvalue');
 
