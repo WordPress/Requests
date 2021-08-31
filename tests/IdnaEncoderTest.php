ee8fe532a7b457a0ee8700cd7cea590a21b3e311
jrfnl <jrfnl@users.noreply.github.com>
2021-08-31 05:39:38 +0200

    IdnaEncoderTest: refactor encoding tests
    * Merge five tests doing the exact same thing, into one test using a data provider with named data sets and keyed data entries.
    * Remove `static` keyword from data provider.
    * Add docblock documentation to the test and the data provider.

--- tests/IdnaEncoderTest.php
+++ tests/IdnaEncoderTest.php
@@ -7,25 +7,54 @@ use WpOrg\Requests\IdnaEncoder;
 use WpOrg\Requests\Tests\TestCase;
 
 final class IdnaEncoderTest extends TestCase {
-	public static function specExamples() {
-		return array(
-			array(
-				"\xe4\xbb\x96\xe4\xbb\xac\xe4\xb8\xba\xe4\xbb\x80\xe4\xb9\x88\xe4\xb8\x8d\xe8\xaf\xb4\xe4\xb8\xad\xe6\x96\x87",
-				'xn--ihqwcrb4cv8a8dqg056pqjye',
-			),
-			array(
-				"\x33\xe5\xb9\xb4\x42\xe7\xb5\x84\xe9\x87\x91\xe5\x85\xab\xe5\x85\x88\xe7\x94\x9f",
-				'xn--3B-ww4c5e180e575a65lsy2b',
-			),
-		);
-	}
 
 	/**
-	 * @dataProvider specExamples
+	 * Tests encoding a hostname using Punycode.
+	 *
+	 * @dataProvider dataEncoding
+	 *
+	 * @param string $data     Data to encode.
+	 * @param string $expected Expected function output.
+	 *
+	 * @return void
 	 */
 	public function testEncoding($data, $expected) {
 		$result = IdnaEncoder::encode($data);
 		$this->assertSame($expected, $result);
+	}
+
+	/**
+	 * Data Provider.
+	 *
+	 * @return array
+	 */
+	public function dataEncoding() {
+		return array(
+			'ascii character' => array(
+				'data'     => 'a',
+				'expected' => 'a',
+			),
+			'two-byte character' => array(
+				'data'     => "\xc2\xb6", // Pilcrow character
+				'expected' => 'xn--tba',
+			),
+			'three-byte character' => array(
+				'data'     => "\xe2\x82\xac", // Euro symbol
+				'expected' => 'xn--lzg',
+			),
+			'four-byte character' => array(
+				'data'     => "\xf0\xa4\xad\xa2", // Chinese symbol?
+				'expected' => 'xn--ww6j',
+			),
+			'example from specs: RFC3492, section 7.1-B: Simplified Chinese' => array(
+				'data'     => "\xe4\xbb\x96\xe4\xbb\xac\xe4\xb8\xba\xe4\xbb\x80\xe4\xb9\x88\xe4\xb8\x8d\xe8\xaf\xb4\xe4\xb8\xad\xe6\x96\x87",
+				'expected' => 'xn--ihqwcrb4cv8a8dqg056pqjye',
+			),
+			'example from specs: RFC3492, section 7.1-L: Japanese artist' => array(
+				'data'     => "\x33\xe5\xb9\xb4\x42\xe7\xb5\x84\xe9\x87\x91\xe5\x85\xab\xe5\x85\x88\xe7\x94\x9f",
+				'expected' => 'xn--3B-ww4c5e180e575a65lsy2b',
+			),
+		);
 	}
 
 	public function testASCIITooLong() {
@@ -46,26 +75,6 @@ final class IdnaEncoderTest extends TestCase {
 		$this->expectException(Exception::class);
 		$this->expectExceptionMessage('Provided string begins with ACE prefix');
 		IdnaEncoder::encode("xn--\xe4\xbb\x96");
-	}
-
-	public function testASCIICharacter() {
-		$result = IdnaEncoder::encode('a');
-		$this->assertSame('a', $result);
-	}
-
-	public function testTwoByteCharacter() {
-		$result = IdnaEncoder::encode("\xc2\xb6"); // Pilcrow character
-		$this->assertSame('xn--tba', $result);
-	}
-
-	public function testThreeByteCharacter() {
-		$result = IdnaEncoder::encode("\xe2\x82\xac"); // Euro symbol
-		$this->assertSame('xn--lzg', $result);
-	}
-
-	public function testFourByteCharacter() {
-		$result = IdnaEncoder::encode("\xf0\xa4\xad\xa2"); // Chinese symbol?
-		$this->assertSame('xn--ww6j', $result);
 	}
 
 	public function testFiveByteCharacter() {
