f5f3d21875a9421318fff212dc0189d40540bf32
jrfnl <jrfnl@users.noreply.github.com>
2021-09-02 23:20:26 +0200

    HeadersTest: rearrange the method order in the test class
    ... grouping tests related to array access together.

--- tests/Response/HeadersTest.php
+++ tests/Response/HeadersTest.php
@@ -7,6 +7,15 @@ use WpOrg\Requests\Response\Headers;
 use WpOrg\Requests\Tests\TestCase;
 
 final class HeadersTest extends TestCase {
+
+	public function testInvalidKey() {
+		$this->expectException(Exception::class);
+		$this->expectExceptionMessage('Object is a dictionary, not a list');
+
+		$headers   = new Headers();
+		$headers[] = 'text/plain';
+	}
+
 	public function testArrayAccess() {
 		$headers                 = new Headers();
 		$headers['Content-Type'] = 'text/plain';
@@ -19,6 +28,14 @@ final class HeadersTest extends TestCase {
 
 		$this->assertSame('text/plain', $headers['CONTENT-TYPE']);
 		$this->assertSame('text/plain', $headers['content-type']);
+	}
+
+	public function testMultipleHeaders() {
+		$headers           = new Headers();
+		$headers['Accept'] = 'text/html;q=1.0';
+		$headers['Accept'] = '*/*;q=0.1';
+
+		$this->assertSame('text/html;q=1.0,*/*;q=0.1', $headers['Accept']);
 	}
 
 	/**
@@ -41,20 +58,5 @@ final class HeadersTest extends TestCase {
 					throw new Exception('Invalid name: ' . $name);
 			}
 		}
-	}
-
-	public function testInvalidKey() {
-		$this->expectException(Exception::class);
-		$this->expectExceptionMessage('Object is a dictionary, not a list');
-		$headers   = new Headers();
-		$headers[] = 'text/plain';
-	}
-
-	public function testMultipleHeaders() {
-		$headers           = new Headers();
-		$headers['Accept'] = 'text/html;q=1.0';
-		$headers['Accept'] = '*/*;q=0.1';
-
-		$this->assertSame('text/html;q=1.0,*/*;q=0.1', $headers['Accept']);
 	}
 }
