60dda54111993ee0bf4296ea7bbb1ce58f32ce75
jrfnl <jrfnl@users.noreply.github.com>
2021-09-18 02:11:31 +0200

    Response::json(): update to comply with coding standards

--- src/Response.php
+++ src/Response.php
@@ -136,18 +136,18 @@ class Response {
 	 */
 	public function json($assoc = true, $depth = 512, $options = 0) {
 		static $json_errors = array(
-			JSON_ERROR_DEPTH => 'JSON_ERROR_DEPTH - Maximum stack depth exceeded',
+			JSON_ERROR_DEPTH          => 'JSON_ERROR_DEPTH - Maximum stack depth exceeded',
 			JSON_ERROR_STATE_MISMATCH => 'JSON_ERROR_STATE_MISMATCH - Underflow or the modes mismatch',
-			JSON_ERROR_CTRL_CHAR => 'JSON_ERROR_CTRL_CHAR - Unexpected control character found',
-			JSON_ERROR_SYNTAX => 'JSON_ERROR_SYNTAX - Syntax error, malformed JSON',
-			JSON_ERROR_UTF8 => 'JSON_ERROR_UTF8 - Malformed UTF-8 characters, possibly incorrectly encoded'
+			JSON_ERROR_CTRL_CHAR      => 'JSON_ERROR_CTRL_CHAR - Unexpected control character found',
+			JSON_ERROR_SYNTAX         => 'JSON_ERROR_SYNTAX - Syntax error, malformed JSON',
+			JSON_ERROR_UTF8           => 'JSON_ERROR_UTF8 - Malformed UTF-8 characters, possibly incorrectly encoded',
 		);
 
 		$data = json_decode($this->body, $assoc, $depth, $options);
 
-		if (JSON_ERROR_NONE !== json_last_error()) {
+		if (json_last_error() !== JSON_ERROR_NONE) {
 			$last_error = json_last_error();
-			$error = isset($json_errors[$last_error]) ? $json_errors[$last_error] : 'Unknown error';
+			$error      = isset($json_errors[$last_error]) ? $json_errors[$last_error] : 'Unknown error';
 			throw new Exception('Unable to parse JSON data: ' . $error, 'response.invalid', $this);
 		}
 
--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -6,30 +6,29 @@ use WpOrg\Requests\Exception;
 use WpOrg\Requests\Response;
 use WpOrg\Requests\Tests\TestCase;
 
-class ResponseTest extends TestCase {
+final class ResponseTest extends TestCase {
 
-    public function testInvalidJsonResponse() {
-        $this->expectException(Exception::class);
+	public function testInvalidJsonResponse() {
+		$this->expectException(Exception::class);
 
-        $response = new Response();
-        $response->body = 'Invalid JSON';
-        $response->json();
-    }
+		$response       = new Response();
+		$response->body = 'Invalid JSON';
+		$response->json();
+	}
 
-    public function testJsonResponse() {
-        $response = new Response();
-        $response->body = '{"success": false, "error": [], "data": null}';
-        $decodedBody = $response->json();
+	public function testJsonResponse() {
+		$response       = new Response();
+		$response->body = '{"success": false, "error": [], "data": null}';
+		$decoded_body   = $response->json();
 
-        $expected = array(
-            'success' => false,
-            'error' => array(),
-            'data' => null
-        );
+		$expected = array(
+			'success' => false,
+			'error'   => array(),
+			'data'    => null,
+		);
 
-        foreach($expected as $key => $value)
-        {
-            $this->assertEquals($value, $decodedBody[$key]);
-        }
-    }
+		foreach ($expected as $key => $value) {
+			$this->assertEquals($value, $decoded_body[$key]);
+		}
+	}
 }
