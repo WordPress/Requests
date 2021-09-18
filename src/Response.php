c5e76756f6dafa9323a5091b8b4f77bb93c84adf
jrfnl <jrfnl@users.noreply.github.com>
2021-09-18 02:36:46 +0200

    Response::decode_body(): modernize the error handling
    As of PHP 5.5.0, PHP contains the `json_last_error_msg()` function which will return the last error message.
    
    This removes the need for the error decoding array, which was outdated by now anyway due to new error codes having been introduced in PHP since the PR was originally pulled.
    
    Ref: https://www.php.net/manual/en/function.json-last-error-msg.php

--- src/Response.php
+++ src/Response.php
@@ -135,20 +135,11 @@ class Response {
 	 * @return array
 	 */
 	public function decode_body($associative = true, $depth = 512, $options = 0) {
-		static $json_errors = array(
-			JSON_ERROR_DEPTH          => 'JSON_ERROR_DEPTH - Maximum stack depth exceeded',
-			JSON_ERROR_STATE_MISMATCH => 'JSON_ERROR_STATE_MISMATCH - Underflow or the modes mismatch',
-			JSON_ERROR_CTRL_CHAR      => 'JSON_ERROR_CTRL_CHAR - Unexpected control character found',
-			JSON_ERROR_SYNTAX         => 'JSON_ERROR_SYNTAX - Syntax error, malformed JSON',
-			JSON_ERROR_UTF8           => 'JSON_ERROR_UTF8 - Malformed UTF-8 characters, possibly incorrectly encoded',
-		);
-
 		$data = json_decode($this->body, $associative, $depth, $options);
 
 		if (json_last_error() !== JSON_ERROR_NONE) {
-			$last_error = json_last_error();
-			$error      = isset($json_errors[$last_error]) ? $json_errors[$last_error] : 'Unknown error';
-			throw new Exception('Unable to parse JSON data: ' . $error, 'response.invalid', $this);
+			$last_error = json_last_error_msg();
+			throw new Exception('Unable to parse JSON data: ' . $last_error, 'response.invalid', $this);
 		}
 
 		return $data;
