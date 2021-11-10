135d8122cc57bc8bb99db95d61a9609b46ed4013
Alain Schlesser <alain.schlesser@gmail.com>
2021-11-10 16:09:15 +0100

    DecompressionTest: add comments to explain how to create new test cases

--- tests/Requests/DecompressionTest.php
+++ tests/Requests/DecompressionTest.php
@@ -90,11 +90,19 @@ final class DecompressionTest extends TestCase {
 	 */
 	public function dataGzip() {
 		return array(
+			/*
+			 * Test data generated using CLI command:
+			 * php -r 'echo "\\x" . substr(chunk_split(bin2hex(gzencode("foobar")),2,"\\x"), 0, -2) . "\n";'
+			 */
 			'gzip: foobar' => array(
 				'expected'   => 'foobar',
 				'compressed' => "\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\x4b\xcb\xcf\x4f\x4a"
 							. "\x2c\x02\x00\x95\x1f\xf6\x9e\x06\x00\x00\x00",
 			),
+			/*
+			 * Test data generated using CLI command:
+			 * php -r 'echo "\\x" . substr(chunk_split(bin2hex(gzencode("Requests for PHP")),2,"\\x"), 0, -2) . "\n";'
+			 */
 			'gzip: Requests for PHP' => array(
 				'expected'   => 'Requests for PHP',
 				'compressed' => "\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\x0b\x4a\x2d\x2c\x4d"
@@ -111,11 +119,13 @@ final class DecompressionTest extends TestCase {
 	 */
 	public function dataDeflate() {
 		return array(
+			// TODO: What is this byte stream representing? Looks like GZIP header with ZLIB compressed data...
 			'deflate: foobar' => array(
 				'expected'   => 'foobar',
 				'compressed' => "\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\x78\x9c\x4b\xcb\xcf"
-					. "\x4f\x4a\x2c\x02\x00\x08\xab\x02\x7a",
+							. "\x4f\x4a\x2c\x02\x00\x08\xab\x02\x7a",
 			),
+			// TODO: What is this byte stream representing? Looks like GZIP header with ZLIB compressed data...
 			'deflate: Requests for PHP' => array(
 				'expected'   => 'Requests for PHP',
 				'compressed' => "\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\x78\x9c\x0b\x4a\x2d"
@@ -132,25 +142,45 @@ final class DecompressionTest extends TestCase {
 	 */
 	public function dataDeflateWithoutHeaders() {
 		return array(
+			/*
+			 * Test data generated using CLI command:
+			 * php -r 'echo "\\x" . substr(chunk_split(bin2hex(gzcompress("foobar")),2,"\\x"), 0, -2) . "\n";'
+			 */
 			'deflate without zlib headers: foobar' => array(
 				'expected'   => 'foobar',
 				'compressed' => "\x78\x9c\x4b\xcb\xcf\x4f\x4a\x2c\x02\x00\x08\xab\x02\x7a",
 			),
+			/*
+			 * Test data generated using CLI command:
+			 * php -r 'echo "\\x" . substr(chunk_split(bin2hex(gzcompress("Requests for PHP")),2,"\\x"), 0, -2) . "\n";'
+			 */
 			'deflate without zlib headers: Requests for PHP' => array(
 				'expected'   => 'Requests for PHP',
 				'compressed' => "\x78\x9c\x0b\x4a\x2d\x2c\x4d\x2d\x2e\x29\x56\x48\xcb\x2f\x52"
 							. "\x08\xf0\x08\x00\x00\x34\x68\x05\xcc",
 			),
+			/*
+			 * Test data generated using CLI command:
+			 * php -r 'echo "\\x" . substr(chunk_split(bin2hex(gzcompress("compression level 1", 1)),2,"\\x"), 0, -2) . "\n";'
+			 */
 			'deflate without zlib headers: compression level 1' => array(
 				'expected'   => 'compression level 1',
 				'compressed' => "\x78\x01\x4b\xce\xcf\x2d\x28\x4a\x2d\x2e\xce\xcc\xcf\x53\xc8\x49"
 							. "\x2d\x4b\xcd\x51\x30\x04\x00\x4d\x86\x07\x3c",
 			),
+			/*
+			 * Test data generated using CLI command:
+			 * php -r 'echo "\\x" . substr(chunk_split(bin2hex(gzcompress("compression level 3", 3)),2,"\\x"), 0, -2) . "\n";'
+			 */
 			'deflate without zlib headers: compression level 3' => array(
 				'expected'   => 'compression level 3',
 				'compressed' => "\x78\x5e\x4b\xce\xcf\x2d\x28\x4a\x2d\x2e\xce\xcc\xcf\x53\xc8\x49"
 							. "\x2d\x4b\xcd\x51\x30\x06\x00\x4d\x88\x07\x3e",
 			),
+			/*
+			 * Test data generated using CLI command:
+			 * php -r 'echo "\\x" . substr(chunk_split(bin2hex(gzcompress("compression level 9", 9)),2,"\\x"), 0, -2) . "\n";'
+			 */
 			'deflate without zlib headers: compression level 9' => array(
 				'expected'   => 'compression level 9',
 				'compressed' => "\x78\xda\x4b\xce\xcf\x2d\x28\x4a\x2d\x2e\xce\xcc\xcf\x53\xc8\x49"
