e102d422c32d1cbbbcc6c42f185e14611c81b6d7
Viktor Szépe <viktor@szepe.net>
2021-11-12 23:51:15 +0100

    Fix type in Request

--- src/Requests.php
+++ src/Requests.php
@@ -621,7 +621,7 @@ class Requests {
 	 * @param array|null $data Data to send either as a query string for GET/HEAD requests, or in the body for POST requests
 	 * @param string $type HTTP request type
 	 * @param array $options Options for the request
-	 * @return array $options
+	 * @return void
 	 */
 	protected static function set_defaults(&$url, &$headers, &$data, &$type, &$options) {
 		if (!preg_match('/^http(s)?:\/\//i', $url, $matches)) {
