88b812e42597d5c9c2856abccc432f495ba1c1c9
Heinz Wiesinger <pprkut@liwjatan.org>
2021-12-17 20:27:10 +0100

    Requests: Fix style issues

--- src/Requests.php
+++ src/Requests.php
@@ -471,12 +471,10 @@ class Requests {
 			$options['hooks']->dispatch('requests.before_parse', [&$response, $url, $headers, $data, $type, $options]);
 
 			$parsed_response = self::parse_response($response, $url, $headers, $data, $options);
-		}
-		catch (Exception $e) {
+		} catch (Exception $e) {
 			$options['hooks']->dispatch('requests.failed', [$e, $url, $headers, $data, $type, $options]);
 			throw $e;
-		}
-		catch (InvalidArgument $e) {
+		} catch (InvalidArgument $e) {
 			$options['hooks']->dispatch('requests.failed', [$e, $url, $headers, $data, $type, $options]);
 			throw $e;
 		}
