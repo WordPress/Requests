2820b33ef6e404113d147d4cd8102383d8d6f776
jrfnl <jrfnl@users.noreply.github.com>
2022-04-03 23:51:23 +0200

    CookieTest: split constructor specific tests to dedicated test class

--- tests/Cookie/CookieTest.php
+++ tests/Cookie/CookieTest.php
@@ -595,133 +595,6 @@ final class CookieTest extends TestCase {
 	}
 
 	/**
-	 * Tests receiving an exception when the constructor received an invalid input type as `$name`.
-	 *
-	 * @dataProvider dataInvalidStringInput
-	 *
-	 * @covers \WpOrg\Requests\Cookie::__construct
-	 *
-	 * @param mixed $input Invalid parameter input.
-	 *
-	 * @return void
-	 */
-	public function testConstructorInvalidName($input) {
-		$this->expectException(InvalidArgument::class);
-		$this->expectExceptionMessage('Argument #1 ($name) must be of type string');
-
-		new Cookie($input, 'value');
-	}
-
-	/**
-	 * Tests receiving an exception when the constructor received an invalid input type as `$value`.
-	 *
-	 * @dataProvider dataInvalidStringInput
-	 *
-	 * @covers \WpOrg\Requests\Cookie::__construct
-	 *
-	 * @param mixed $input Invalid parameter input.
-	 *
-	 * @return void
-	 */
-	public function testConstructorInvalidValue($input) {
-		$this->expectException(InvalidArgument::class);
-		$this->expectExceptionMessage('Argument #2 ($value) must be of type string');
-
-		new Cookie('name', $input);
-	}
-
-	/**
-	 * Data Provider.
-	 *
-	 * @return array
-	 */
-	public function dataInvalidStringInput() {
-		return TypeProviderHelper::getAllExcept(TypeProviderHelper::GROUP_STRING);
-	}
-
-	/**
-	 * Tests receiving an exception when the constructor received an invalid input type as `$name`.
-	 *
-	 * @dataProvider dataConstructorInvalidAttributes
-	 *
-	 * @covers \WpOrg\Requests\Cookie::__construct
-	 *
-	 * @param mixed $input Invalid parameter input.
-	 *
-	 * @return void
-	 */
-	public function testConstructorInvalidAttributes($input) {
-		$this->expectException(InvalidArgument::class);
-		$this->expectExceptionMessage('Argument #3 ($attributes) must be of type array|ArrayAccess&Traversable');
-
-		new Cookie('name', 'value', $input);
-	}
-
-	/**
-	 * Data Provider.
-	 *
-	 * @return array
-	 */
-	public function dataConstructorInvalidAttributes() {
-		$except = array_intersect(TypeProviderHelper::GROUP_ITERABLE, TypeProviderHelper::GROUP_ARRAY_ACCESSIBLE);
-		return TypeProviderHelper::getAllExcept($except);
-	}
-
-	/**
-	 * Tests receiving an exception when the constructor received an invalid input type as `$flags`.
-	 *
-	 * @dataProvider dataConstructorInvalidFlags
-	 *
-	 * @covers \WpOrg\Requests\Cookie::__construct
-	 *
-	 * @param mixed $input Invalid parameter input.
-	 *
-	 * @return void
-	 */
-	public function testConstructorInvalidFlags($input) {
-		$this->expectException(InvalidArgument::class);
-		$this->expectExceptionMessage('Argument #4 ($flags) must be of type array');
-
-		new Cookie('name', 'value', [], $input);
-	}
-
-	/**
-	 * Data Provider.
-	 *
-	 * @return array
-	 */
-	public function dataConstructorInvalidFlags() {
-		return TypeProviderHelper::getAllExcept(TypeProviderHelper::GROUP_ARRAY);
-	}
-
-	/**
-	 * Tests receiving an exception when the constructor received an invalid input type as `$reference_time`.
-	 *
-	 * @dataProvider dataConstructorInvalidReferenceTime
-	 *
-	 * @covers \WpOrg\Requests\Cookie::__construct
-	 *
-	 * @param mixed $input Invalid parameter input.
-	 *
-	 * @return void
-	 */
-	public function testConstructorInvalidReferenceTime($input) {
-		$this->expectException(InvalidArgument::class);
-		$this->expectExceptionMessage('Argument #5 ($reference_time) must be of type integer|null');
-
-		new Cookie('name', 'value', [], [], $input);
-	}
-
-	/**
-	 * Data Provider.
-	 *
-	 * @return array
-	 */
-	public function dataConstructorInvalidReferenceTime() {
-		return TypeProviderHelper::getAllExcept(TypeProviderHelper::GROUP_NULL, TypeProviderHelper::GROUP_INT);
-	}
-
-	/**
 	 * Tests receiving an exception when the parse() method received an invalid input type as `$cookie_header`.
 	 *
 	 * @dataProvider dataInvalidStringInput
@@ -755,6 +628,15 @@ final class CookieTest extends TestCase {
 		$this->expectExceptionMessage('Argument #2 ($name) must be of type string');
 
 		Cookie::parse('test', $input);
+	}
+
+	/**
+	 * Data Provider.
+	 *
+	 * @return array
+	 */
+	public function dataInvalidStringInput() {
+		return TypeProviderHelper::getAllExcept(TypeProviderHelper::GROUP_STRING);
 	}
 
 	/**
