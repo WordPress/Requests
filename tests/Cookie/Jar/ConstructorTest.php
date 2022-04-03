0d23c46c0f5c536c41316c3dbc62e9719d49ab0c
jrfnl <jrfnl@users.noreply.github.com>
2022-04-03 23:22:53 +0200

    Cookie\Jar\ConstructorTest: add extra test specifically for the constructor
    ... to make the `Cookie\Jar\ConstructorTest` feature complete and independent of the other tests for this class.

--- tests/Cookie/Jar/ConstructorTest.php
+++ tests/Cookie/Jar/ConstructorTest.php
@@ -2,6 +2,7 @@
 
 namespace WpOrg\Requests\Tests\Cookie\Jar;
 
+use ReflectionObject;
 use WpOrg\Requests\Cookie\Jar;
 use WpOrg\Requests\Exception\InvalidArgument;
 use WpOrg\Requests\Tests\TestCase;
@@ -35,5 +36,35 @@ final class ConstructorTest extends TestCase {
 	 */
 	public function dataInvalidInputType() {
 		return TypeProviderHelper::getAllExcept(TypeProviderHelper::GROUP_ARRAY);
+	}
+
+	/**
+	 * Tests that valid data is accepted by the constructor and the property gets set.
+	 *
+	 * @dataProvider dataValidInputType
+	 *
+	 * @param mixed $input Valid parameter input.
+	 *
+	 * @return void
+	 */
+	public function testValidInputType($input) {
+		$obj = new Jar($input);
+
+		$reflection = new ReflectionObject($obj);
+		$property   = $reflection->getProperty('cookies');
+		$property->setAccessible(true);
+		$property_value = $property->getValue($obj);
+		$property->setAccessible(false);
+
+		$this->assertSame($input, $property_value, 'Cookies property has not been set to expected value');
+	}
+
+	/**
+	 * Data Provider.
+	 *
+	 * @return array
+	 */
+	public function dataValidInputType() {
+		return TypeProviderHelper::getSelection(TypeProviderHelper::GROUP_ARRAY);
 	}
 }
