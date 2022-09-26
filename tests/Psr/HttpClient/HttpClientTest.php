020664c435fced7f6f73673243ea1fb3b6e3aee4
Art4 <art4@wlabs.de>
2022-09-26 09:17:03 +0200

    Add tests for receifing response on 404 or 503 error

--- tests/Psr/HttpClient/HttpClientTest.php
+++ tests/Psr/HttpClient/HttpClientTest.php
@@ -50,6 +50,80 @@ final class HttpClientTest extends TestCase {
 	}
 
 	/**
+	 * Tests receiving a response when using sendRequest().
+	 *
+	 * @covers \WpOrg\Requests\Psr\HttpClient::sendRequest
+	 *
+	 * @return void
+	 */
+	public function testSendRequestReturnsResponseOn404Error() {
+		$transport = $this->createMock(Transport::class);
+		$transport->expects($this->once())->method('request')->willReturnCallback(function ($url, $headers, $data, $options) use ($transport) {
+			$this->assertSame('https://example.org/not-found', $url);
+			$this->assertSame(['Host' => ['example.org']], $headers);
+			$this->assertSame('', $data);
+			$this->assertSame('GET', $options['type']);
+
+			return
+				'HTTP/1.1 404 Not Found' . "\r\n".
+				'Content-Type:text/plain'. "\r\n".
+				"\r\n".
+				'404 Not Found';
+		});
+
+		$httpClient = new HttpClient([
+			'transport' => $transport,
+		]);
+
+		$request = $httpClient->createRequest('GET', 'https://example.org/not-found');
+
+		$response = $httpClient->sendRequest($request);
+
+		$this->assertSame(404, $response->getStatusCode());
+		$this->assertSame('Not Found', $response->getReasonPhrase());
+		$this->assertSame('1.1', $response->getProtocolVersion());
+		$this->assertSame(['content-type' => ['text/plain']], $response->getHeaders());
+		$this->assertSame('404 Not Found', $response->getBody()->__toString());
+	}
+
+	/**
+	 * Tests receiving a response when using sendRequest().
+	 *
+	 * @covers \WpOrg\Requests\Psr\HttpClient::sendRequest
+	 *
+	 * @return void
+	 */
+	public function testSendRequestReturnsResponseOn503Error() {
+		$transport = $this->createMock(Transport::class);
+		$transport->expects($this->once())->method('request')->willReturnCallback(function ($url, $headers, $data, $options) use ($transport) {
+			$this->assertSame('https://example.org/not-available', $url);
+			$this->assertSame(['Host' => ['example.org']], $headers);
+			$this->assertSame('', $data);
+			$this->assertSame('GET', $options['type']);
+
+			return
+				'HTTP/1.1 503 Service Unavailable' . "\r\n".
+				'Content-Type:text/plain'. "\r\n".
+				"\r\n".
+				'503 Service Unavailable';
+		});
+
+		$httpClient = new HttpClient([
+			'transport' => $transport,
+		]);
+
+		$request = $httpClient->createRequest('GET', 'https://example.org/not-available');
+
+		$response = $httpClient->sendRequest($request);
+
+		$this->assertSame(503, $response->getStatusCode());
+		$this->assertSame('Service Unavailable', $response->getReasonPhrase());
+		$this->assertSame('1.1', $response->getProtocolVersion());
+		$this->assertSame(['content-type' => ['text/plain']], $response->getHeaders());
+		$this->assertSame('503 Service Unavailable', $response->getBody()->__toString());
+	}
+
+	/**
 	 * Tests receiving an exception when using sendRequest().
 	 *
 	 * @covers \WpOrg\Requests\Psr\HttpClient::sendRequest
