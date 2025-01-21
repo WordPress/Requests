360d9e27e50b669b276e70938fb4419baa91d88e
Alain Schlesser <alain.schlesser@gmail.com>
2025-01-21 12:18:41 +0100

    Adapt argument support in PHPUnit script

--- scripts/run-phpunit.sh
+++ scripts/run-phpunit.sh
@@ -14,15 +14,9 @@ fi
 # Detect PHPUnit version
 PHPUNIT_VERSION=$("${PROJECT_ROOT}/vendor/bin/phpunit" --version | grep -oE '[0-9]+\.[0-9]+' | head -n1)
 
-# Determine if we should include coverage based on argument
-COVERAGE_ARG=""
-if [ "$1" != "--no-coverage" ]; then
-    COVERAGE_ARG="--coverage-html build/coverage"
-fi
-
 # Run the tests with the appropriate config
 if printf '%s\n%s\n' "10.0" "$PHPUNIT_VERSION" | sort -V -C 2>/dev/null; then
-    "${PROJECT_ROOT}/vendor/bin/phpunit" -c phpunit10.xml.dist $COVERAGE_ARG
+    "${PROJECT_ROOT}/vendor/bin/phpunit" -c phpunit10.xml.dist "$1"
 else
-    "${PROJECT_ROOT}/vendor/bin/phpunit" $COVERAGE_ARG
+    "${PROJECT_ROOT}/vendor/bin/phpunit" "$1"
 fi
