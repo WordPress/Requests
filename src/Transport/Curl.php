3dfeb427f2ae2422d1a56e957fac5d247c4903fa
jrfnl <jrfnl@users.noreply.github.com>
2025-11-21 12:45:09 +0100

    CS: start using PHPCompatibility 10
    Long anticipated, finally here: PHPCompatibility 10.0.0-alpha1 :tada:
    
    PHPCompatibility 10.0.0 brings huge improvements in both what is being detected (> 50 new sniffs), as well as the detection accuracy for pre-existing sniffs.
    
    Even though still "unstable", it is stable enough for our purposes and the advantages of using it outweight the disadvantage of it being an unstable version. By setting the `minimum-stability` and `prefer-stable` settings in the `composer.json`, we can ensure that we don't get the `dev-develop` branch, but rather get a `10.0.0` tag, unstable or not.
    
    And what with the improved detection, a number of php incompatibilities previously not flagged, are not flagged, even though we already handle them correctly via conditions.
    So this commit also adds a few selective ignore comments for those few situations where they are needed.
    
    Ref:
    * https://github.com/PHPCompatibility/PHPCompatibility/wiki/Upgrading-to-PHPCompatibility-10.0
    * https://github.com/PHPCompatibility/PHPCompatibility/releases/tag/10.0.0-alpha1

--- src/Transport/Curl.php
+++ src/Transport/Curl.php
@@ -126,6 +126,7 @@ final class Curl implements Transport {
 	 */
 	public function __destruct() {
 		if (is_resource($this->handle)) {
+			// phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.curl_closeDeprecated,Generic.PHP.DeprecatedFunctions.Deprecated
 			curl_close($this->handle);
 		}
 	}
@@ -307,6 +308,7 @@ final class Curl implements Transport {
 
 				curl_multi_remove_handle($multihandle, $done['handle']);
 				if (is_resource($done['handle'])) {
+					// phpcs:ignore PHPCompatibility.FunctionUse.RemovedFunctions.curl_closeDeprecated,Generic.PHP.DeprecatedFunctions.Deprecated
 					curl_close($done['handle']);
 				}
 
